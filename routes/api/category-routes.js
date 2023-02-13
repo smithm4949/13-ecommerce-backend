06bf1b02792f95078548ad02b76c5d965194274a
smithm4949 <smithm4949@gmail.com>
2023-02-13 12:45:08 -0500

    categories get methods added ('/' and '/:id')

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,32 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: Product
+  })
+  .then((categories) => {
+    res.status(200).json(categories);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {id: req.params.id},
+    include: Product
+  })
+  .then((category) => {
+    res.status(200).json(category);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
