8d1f3814e4561b43ac214a2ed6fd7fdc82dbb67b
smithm4949 <smithm4949@gmail.com>
2023-02-13 12:57:19 -0500

    all routes added and working for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
   .catch((err) => {
     console.log(err);
     res.status(400).json(err);
-  })
+  });
 });
 
 router.get('/:id', (req, res) => {
@@ -31,19 +31,51 @@ router.get('/:id', (req, res) => {
   .catch((err) => {
     console.log(err);
     res.status(400).json(err);
-  })
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then((category) => {
+    res.status(200).json(category);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((category) => {
+    res.status(200).json(category);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((category) => {
+    res.status(200).json(category);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 module.exports = router;
